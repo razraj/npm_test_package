d7bc2c77313c202f8de357b2b55bf2f122e4e19b
rraikarnvi <raikarrajat@gmail.com>
2022-11-27 17:52:09 +0530

    1.0.1v4

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { MyCounter } from 'my-react-typescript-package'
+import { MyCounter } from 'r_npm_test_package'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
